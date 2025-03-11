4e6c57e11166927f2d720e637fe8cb46e3ec63c9
FMF95 <81781009+FMF95@users.noreply.github.com>
2025-03-11 23:59:56 +0100

    11.script_set_rbe3_cte.tcl
    Script that sets the CTE of the displayed RBE3 elements.

---
+++ 11.script_set_rbe3_cte.tcl
@@ -0,0 +1,24 @@
+clear
+*clearmark elems 1
+*createmark elems 1 "displayed"
+
+set element_list [hm_getmark elems 1]
+
+set cte [hm_getfloat "Introduce the CTE value to set for the displayed RBE3"]
+puts "The value of the CTE is: $cte"
+puts " working on it... "
+
+foreach element_id $element_list {
+
+    *startnotehistorystate {Attached attributes to element}
+    *attributeupdateint elements $element_id 3240 1 2 0 1
+    *attributeupdateint elements $element_id 4061 1 2 0 0
+    *attributeupdateint elements $element_id 4660 1 2 0 1
+    *attributeupdatedouble elements $element_id 4659 1 1 0 $cte
+    *endnotehistorystate {Attached attributes to element}
+
+}
+
+puts "All CTE from the displayed RBE3 are modified."
+*clearmark elems 1
+bell
